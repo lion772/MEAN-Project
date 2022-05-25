07e5baba00a7a0bba4e81f0746a137813528bff0
william <william.steinkedemello@gmail.com>
2022-05-25 22:39:19 +0200

    create onAddPost and onKey functions in post-create component

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -4,4 +4,21 @@ import { Component } from '@angular/core';
   selector: 'app-post-create',
   templateUrl: './post-create.component.html',
 })
-export class PostCreateComponent {}
+export class PostCreateComponent {
+  values: string = '';
+  //@HostListener('click', ['$event']) onClick(event) {
+  //  console.log('component is clicked');
+  //  console.log(event);
+  // }
+
+  onAddPost() {
+    console.log('save button clicked');
+    console.log(this.values);
+  }
+
+  onKey(event: any) {
+    // without type info
+    this.values += event.target.value;
+    // console.log(this.values);
+  }
+}
