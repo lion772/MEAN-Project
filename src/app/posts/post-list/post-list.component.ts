7590a27d3e2442a161710b06f32f53276c50e0ea
william <william.steinkedemello@gmail.com>
2022-05-27 22:41:23 +0200

    add directives ngFor and ngIf to loop through a list directly in the DOM and create a condition, respectively

---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css'],
+})
+export class PostListComponent {
+  //posts = [
+  //  { title: 'First Post', content: 'First Content' },
+  //  { title: 'Second Post', content: 'Second Content' },
+  //  { title: 'Third Post', content: 'Third Content' },
+  //  { title: 'Fourth Post', content: 'Fourth Content' },
+  //];
+  posts = [];
+}
