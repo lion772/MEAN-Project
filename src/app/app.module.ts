1d2213ddb4ac8a968d5cd45c86731af6702bd31e
william <william.steinkedemello@gmail.com>
2022-05-27 09:32:14 +0200

    Header component finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [AppComponent, PostCreateComponent, HeaderComponent],
@@ -20,6 +21,7 @@ import { MatButtonModule } from '@angular/material/button';
     MatInputModule,
     MatCardModule,
     MatButtonModule,
+    MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/posts/post-create/post-create.component.spec.ts
@@ -0,0 +1,10 @@
+import { TestBed } from '@angular/core/testing';
+import { PostCreateComponent } from './post-create.component';
+
+describe('PostCreateComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [PostCreateComponent],
+    }).compileComponents();
+  });
+});
