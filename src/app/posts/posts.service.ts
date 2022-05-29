a762700262ec122ff84dc84d33ebb801b20c1ef6
william <william.steinkedemello@gmail.com>
2022-05-29 17:35:40 +0200

    implement observable and observer by adding Subject to posts.service

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
 import { Post } from './post.model';
+//
 import { Subject } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class PostsService {
   private posts: Post[] = [];
-  private postsUpdated = new Subject<Post[]>();
+  private postsUpdated = new Subject<Post[]>(); //Observable
 
   getPosts() {
     //don't want to return the original array. Instead, return a copy to not
@@ -23,3 +24,6 @@ export class PostsService {
     this.postsUpdated.next([...this.posts]); //it pushes and emits a new value
   }
 }
+
+//A Subject is Observable and Observer at the same time. Since it is an Observer, we can emit events with next(). Since it is also an Observable, we can subscribe to these events.
+//With asObservable() we take care that from outside the service it is only accessible as an Observable, so that noone outside the service can accidentally add events to it, only subscribe to it.
