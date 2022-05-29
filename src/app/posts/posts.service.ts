c736789a10eaef70b5c9f1cf79c090b5236cadb6
william <william.steinkedemello@gmail.com>
2022-05-29 09:08:24 +0200

    replace property and event binding with typescript service class

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,5 +1,7 @@
 import { Post } from './post.model';
+import { Injectable } from '@angular/core';
 
+@Injectable({ providedIn: 'root' })
 export class PostsService {
   private posts: Post[] = [];
 
@@ -7,5 +9,14 @@ export class PostsService {
     //don't want to return the original array. Instead, return a copy to not
     //affect the original array and try to be immutable.
     return [...this.posts];
+  }
+
+  //addPosts(title: string, content:string) {
+  //  const post:Post = {title:title, content:content}
+  //  this.posts.push(post);
+  //}
+
+  addPosts(post: Post) {
+    this.posts.push(post);
   }
 }
